46485745199039e1f23f1c9bec9f578bb0cc1125
vksrai96 <vksrai96@gmail.com>
2022-01-17 03:53:03 +0100

    authentication

--- trendz/settings.py
+++ trendz/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'carts.context_processors.counter',
             ],
         },
     },
@@ -131,3 +133,15 @@ STATICFILES_DIRS = [
 #media files configuratio
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER ='vksraiads@gmail.com'
+EMAIL_HOST_PASSWORD = 'Viskas@007'
+EMAIL_USE_TLS = True
